6c5840878b87d6d47cdedfa25f212af8d1da5728
qiang.tu <qiang.tu@renren-inc.com>
2016-09-14 18:23:36 +0800

    增加粒子动画

--- rotate/particle.js
+++ rotate/particle.js
@@ -1,15 +1,142 @@
+// require("./basicLib")
+
 let canvas = {};
 canvas.obj = document.getElementById("world");
 canvas.ctx = canvas.obj.getContext("2d")
 let c = canvas.obj.getContext("2d")
 
+let image = {};
+
+let img = new Image();
 window.requestNextAnimationFrame = () =>{
-    return window.requestAnimationFrame ||10
+    return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.msRequestAnimationFrame
 }
 
+class BasicConfig {
+    drawBackground(){
+        c.save();
+        c.beginPath();
+        c.rect(0,0,canvas.obj.width,canvas.obj.height);
+        c.fillStyle = "#99CCFF";
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+    drawBG(){
+        window.requestAnimationFrame(this.drawBackground);
+    }
+}
+let Basic = new BasicConfig();
+// Basic.drawBG();
+// let stage = new Stage(canvas.obj);
+// rotate3d学习
+class Ball{
+    constructor(data){
+        this.x = data.x;
+        this.y = data.y;
+        this.width = data.width;
+        this.angleY = 0;
+    }
+    draw(){
+        c.save();
+        let r = (Math.random() * 28).toFixed(0);
+        // console.log(r)
+        let g = (Math.random() * 124).toFixed(0);
+        let b = (Math.random() * 200  + 50).toFixed(0);
+        let a = (Math.random().toFixed(0))
+        console.log(b)
+        c.beginPath();
+        c.arc(this.x,this.y,this.width/2,0,Math.PI * 2,true);
+        c.closePath();
+        // c.fillStyle = `rgba(${r},${g},0,1)`;
+        c.fillStyle = `rgba(${r},${g},${b},${Math.max(0.5,a)})`;
+        console.log(c.fillStyle)
+        c.fill();
+    }
+}
+
+
+class Rotate{
+    createBall(radius){
+        radius = (radius === undefined) ? 20: radius;
+        return new Ball({
+            x: 0,
+            y: 0,
+            width: radius * 2,
+        })
+    }
+
+    init(){
+        let xpos = 0,
+            ypos = 0,
+            zpos = 0,
+            focalLength = 250,
+            // 焦距
+            ballR = 25,
+            ballN = 80,
+            balls = [],
+            vpx,
+            vpy,
+            angleY = 20;
+        let ball;
+
+        for(let i=0; i< ballN;i++){
+            ball = this.createBall(ballR);
+            ball.xpos = Math.random() *600;
+            ball.ypos = Math.random() *800 - 500;
+            ball.zpos = Math.random() *300;
+            balls.push(ball)
+        }
+        // 画布中心位置
+        vpx = canvas.obj.width/2;
+        vpy = canvas.obj.height/2;
+        // 取得相对比中心点的位置
+
+        document.addEventListener("mousemove",(e)=>{
+            angleY = (e.x -vpx) * 0.001;
+
+            c.clearRect(0,0,canvas.obj.width,canvas.obj.height)
+            window.requestAnimationFrame(refresh)
+        });
+        let rotateY = (ball)=> {
+            // console.log(angleY)
+
+            let cosy = Math.cos(angleY),
+                siny = Math.sin(angleY),
+                x1 = ball.xpos * cosy - ball.zpos * siny,
+                z1 = ball.zpos * cosy + ball.xpos * siny;
+            ball.xpos = x1;
+            ball.zpos = z1;
+
+            let scale = focalLength /(focalLength + ball.zpos);
+
+            ball.x = vpx + ball.xpos * scale;
+            ball.y = vpy + ball.ypos * scale;
+            // console.log(scale)
+            ball.width = Math.abs(ballR * 2 * scale)
+            ball.draw()
+        }
+
+        let refresh = () =>{
+            balls.forEach( (item)=>{
+                rotateY(item)
+            })
+
+        }
+        window.requestAnimationFrame(refresh)
+    }
+    refresh(balls,angleY){
+
+    }
+
+}
+
+let R = new Rotate();
+//
+// R.init()
 
 class Dot{
     constructor(centerX,centerY,centerZ,radius){
@@ -38,6 +165,11 @@ class Dot{
         let y = canvas.obj.height/2 + (this.y - canvas.obj.height/2) * scale;
         c.arc(x,y,this.radius*scale,0,Math.PI * 2);
         c.fillStyle = `rgba(33,125,198,${scale})`;
+
+        // c.shadowBlur = 2;
+        // c.shadowOffsetX = 0.1;
+        // c.shadowOffsetY = 0.2;
+        // c.shadowColor = "#666";
         c.fill()
         c.restore();
     }
@@ -74,8 +206,8 @@ class ParticleText{
         c.clearRect(0,0,canvas.obj.width,canvas.obj.height);
         this.dots.forEach((item)=>{
             // 判断飞散的粒子是否回到了原位
-            let flag = Math.abs(item.dx - item.x) < 0.1 && Math.abs(item.dy - item.y) < 0.1 && Math.abs(item.dz - item.z) < 0.1;
-            // let flag = item.dx === item.x && item.dy === item.y && item.dz === item.z;
+            // let flag = Math.abs(item.dx - item.x) < 0.1 && Math.abs(item.dy - item.y) < 0.1 && Math.abs(item.dz - item.z) < 0.1;
+            let flag = item.dx === item.x && item.dy === item.y && item.dz === item.z;
             // 粒子是否回到了飞散后的位置
             let flagSecond = Math.abs(item.tx - item.x) < 0.1 && Math.abs(item.ty - item.y)<0.1 && Math.abs(item.tz - item.z) < 0.1;
             const speed = 0.1;
