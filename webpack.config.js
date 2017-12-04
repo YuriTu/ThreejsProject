26c1b9f4cf84eee5c498e48da8e78ff21d73b5da
tuqiang01 <tuqiang01@baidu.com>
2017-12-04 10:59:58 +0800

    update package

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+/**
+ * @file Describe the file
+ * @author 涂强（tuqiang01@baidu.com）
+ */
+
 const webpack = require("webpack");
-
 const path = require("path");
-
 const DashboardPlugin = require("webpack-dashboard/plugin");
-// 入口文件
+
 const entries = {
     "basic"         : "./src/main.js",
     "canvas"        : "./canvas/canvasProject.js",
@@ -16,25 +19,88 @@ const entries = {
 };
 module.exports = {
     entry  : entries,
-    // 输出配置
     output : {
-        filename: "./js/[name].bundle.js",
+        filename   : "[name].bundle.js",
+        path       : path.resolve(__dirname, "dist/js/"),
+        publicPath : "http://localhost:9888/dist/js/"
     },
-    // 加载器配置
     module: {
-        loaders: [
-            { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.js[x]?$/, loader: "babel-loader?presets[]=es2015" }
+        noParse: /jquery/,
+        rules: [
+            {
+                test   : /\.scss$/,
+                use : ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test   : /\.css$/,
+                use : ['style-loader','css-loader'],
+            },
+            {
+                test   : /\.(png|jpg)$/,
+                use : {
+                    loader: "url-loader",
+                    options: {
+                        limit: 8192,
+                    }
+                }
+            },
+            {
+                test    : /\.js[x]?$/,
+                exclude : /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env','stage-0'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            },
+
         ]
     },
-    plugins: [
-        new DashboardPlugin()
+    devtool : "cheap-module-eval-source-map",
+    plugins : [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('dev')
+            }
+        }),
+        new DashboardPlugin(),
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        // new webpack.HotModuleReplacementPlugin(),
+        // new webpack.NoErrorsPlugin()
+    ],
+    node: {
+        console: false,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
+    externals: [
+        'child_process',
+        'aws-sdk'
     ],
     devServer: {
         hot    : true,
-        inline : true
+        inline : true,
+        port:9001,
+        proxy  : {
+            "/api/*": {
+                target       : "http://develop.com",
+                changeOrigin : true,
+                pathRewrite  : {
+                    "^/api": ""
+                }
+            }
+        },
+        headers: {
+            "Access-Control-Allow-Origin"  : "*",
+            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers" : "X-Requested-With, content-type, Authorization"
+        }
     }
+
 };
+
 
 
