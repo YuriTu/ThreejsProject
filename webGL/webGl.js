8d2855399720d2d07fcbd5bf99aead31262e551e
tuqiang01 <tuqiang01@baidu.com>
2018-10-09 19:31:54 +0800

    updateColorPure

--- webGL/webGl.js
+++ webGL/webGl.js
@@ -41,64 +41,60 @@ class Main {
         gl = canvas.getContext('webgl');
         initShaders(gl,vertextshader,fragmentshader);
 
-        this.initDOM = () => {
-            this.text = document.querySelector('#far')
-        }
 
         this.initVertexBuffers = () => {
-            this.verticesColors = new Float32Array([
-                // Vertex coordinates and color
-                1.0,  1.0,  1.0,     1.0,  1.0,  1.0,  // v0 White
-                -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 Magenta
-                -1.0, -1.0,  1.0,     1.0,  0.0,  0.0,  // v2 Red
-                1.0, -1.0,  1.0,     1.0,  1.0,  0.0,  // v3 Yellow
-                1.0, -1.0, -1.0,     0.0,  1.0,  0.0,  // v4 Green
-                1.0,  1.0, -1.0,     0.0,  1.0,  1.0,  // v5 Cyan
-                -1.0,  1.0, -1.0,     0.0,  0.0,  1.0,  // v6 Blue
-                -1.0, -1.0, -1.0,     0.0,  0.0,  0.0   // v7 Black
+            this.vertices = new Float32Array([
+                1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+                1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+                1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+                -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+                -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+                1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
             ]);
+            this.colors = new Float32Array([
+                0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+                0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+                1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+                1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+                1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+                0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+
+            ])
             this.indices = new Uint8Array([
                 0, 1, 2,   0, 2, 3,    // front
-                0, 3, 4,   0, 4, 5,    // right
-                0, 5, 6,   0, 6, 1,    // up
-                1, 6, 7,   1, 7, 2,    // left
-                7, 4, 3,   7, 3, 2,    // down
-                4, 7, 6,   4, 6, 5     // back
+                4, 5, 6,   4, 6, 7,    // right
+                8, 9,10,   8,10,11,    // up
+                12,13,14,  12,14,15,    // left
+                16,17,18,  16,18,19,    // down
+                20,21,22,  20,22,23     // back
             ]);
             this.n = this.indices.length;
-            this.vSize = this.verticesColors.BYTES_PER_ELEMENT;
+            this.vSize = this.vertices.BYTES_PER_ELEMENT;
+        };
+        this.initArrayBuffer = (data, num, type, attribute) => {
+            let buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+            let a_attribute = gl.getAttribLocation(gl.program, attribute);
+            // 通知shader，数据该如何处理
+            gl.vertexAttribPointer(a_attribute, num,type,false,0,0);
+
+            gl.enableVertexAttribArray(a_attribute);
         };
         this.createBuffer = () => {
-            // 创建
-            let vertexColorBuffer  = gl.createBuffer();
-            let indexBuffer = gl.createBuffer();
-            // 注入数据
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER,this.verticesColors,gl.STATIC_DRAW);
-            // 把缓冲区 分配对应的shader变量
-            let location = gl.getAttribLocation(gl.program, 'a_Position');
-            // 告诉shader变量 缓冲区数据情况
-            // 将绑定到gl.array_buffer 的缓冲区对象分配给由location
-            // 指定的shader变量
-            gl.vertexAttribPointer(location,3,gl.FLOAT,false,this.vSize * 6,0);
 
-            let a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-            gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, this.vSize * 6, this.vSize * 3);
-
-            this.u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-            this.mvpMatrix = new Matrix4();
-
-            this.mvpMatrix.setPerspective(30,SCREEN_WIDTH/SCREEN_HEIGHT,1,100);
-            this.mvpMatrix.lookAt(3,3,7,0,0,0,0,1,0);
-
-
-            gl.uniformMatrix4fv(this.u_MvpMatrix, false, this.mvpMatrix.elements);
-
-            // 开启数据
-            gl.enableVertexAttribArray(location);
-            gl.enableVertexAttribArray(a_Color);
+            this.initArrayBuffer(this.vertices, 3, gl.FLOAT, 'a_Position');
+            this.initArrayBuffer(this.colors, 3, gl.FLOAT, 'a_Color');
+            //
+            let u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+            let mvpMatrix = new Matrix4();
+            mvpMatrix.setPerspective(30, 1, 1, 100);
+            mvpMatrix.lookAt(3,3,7, 0,0,0, 0,1,0);
+            gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
 
             // 开始处理链接方式坐标map
+            let indexBuffer = gl.createBuffer();
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,this.indices, gl.STATIC_DRAW);
         }
@@ -109,7 +105,6 @@ class Main {
     init(){
         this.initVertexBuffers();
         this.createBuffer();
-        this.initDOM();
         this.initRender();
     }
 
@@ -118,9 +113,9 @@ class Main {
         gl.clear(gl.COLOR_BUFFER_BIT);
 
         gl.enable(gl.DEPTH_TEST);
-        gl.enable(gl.POLYGON_OFFSET_UNITS);
+        // gl.enable(gl.POLYGON_OFFSET_UNITS);
 
-        gl.polygonOffset(1.0, 1.0);
+        // gl.polygonOffset(1.0, 1.0);
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
